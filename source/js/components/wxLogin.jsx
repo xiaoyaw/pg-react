cd358aaf3a1b26bc93aa53bdc41b9a48a1daa5e0
xiaoyaw <wll77wang@163.com>
2016-11-07 11:22:57 +0800

    react:测试post请求获取数据
    Change-Id: Ida404a38222741f98ff193756aeefacf0d519a02

--- source/js/components/wxLogin.jsx
+++ source/js/components/wxLogin.jsx
@@ -6,85 +6,103 @@ import {
 var React = require('react');
 
 var wxLogin = React.createClass({
-			getInitialState: function() {
-					$('#log').text($('#log').text()+"| begin");
-				return {
-					code:'',
-					isLogin: false
-				};
-			},
-			componentWillMount: function() {
-				var req = new Object();
-				req = this.getRequest();
-				var code = req['code'];
-				if (code != '' && code != undefined) {
-					$('#log').text($('#log').text()+"| code is ok");
-					this.setState({
-						code:code,
-						isLogin: true
-					});
-				};
-			},
-			componentDidMount: function() {
-				$('#log').text($('#log').text()+"| begin to get from php"+this.state.code+"    "+this.state.isLogin);
-				if (this.state.isLogin) {
-					$('#log').text($('#log').text()+"| gggggg"+this.state.code+"    "+this.state.isLogin);
-					$
-						.ajax({
-							async: true,
-							url: "http://pictoshare.net/dev/build/php/oauth2_sub.php",
-							type: "GET",
-							data: {
-								code: this.state.code
-							},
-							timeout: 5000,
-							success: function(result) {
-								
-								var arry = result.split(":");
-								var subscribe=arry[3];
-								this.localSave(arry[2],arry[3],arry[0],arry[1]);
-								$('#log').text($('#log').text()+"|  data is ok ="+arry[2]);
-								if (subscribe == 0 && subscribe != '' && subscribe != undefined && subscribe != 'undefined') {
-									document.location = "http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyNzE3NjM1Nw==&scene=110#&wechat_redirect";
-								} else {
-									browserHistory.replace('/join');
-										$('#log').text($('#log').text()+"|  all is ok");
-								}
-							},
-							error:function(){
-								$('#log').text($('#log').text()+"|  请求错误");
-							}
-						});
-				} else {
-					//修改授权地址
-					document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-				}
-			},
-			localSave: function(n,s,o,t) {
-				if (typeof(Storage) !== "undefined") {
-					sessionStorage.setItem("nickname", n);
-					sessionStorage.setItem("subscribe", s);
-					sessionStorage.setItem("openid", o);
-					sessionStorage.setItem("accesstoken", t);
-				}
-			},
-			getRequest: function() {
-				var url = document.location.search;
-				var theRequest = new Object();
-				var strs;
-				if (url.indexOf("?") != -1) {
-					var str = url.substr(1);
-					strs = str.split("&");
-					for (var i = 0; i < strs.length; i++) {
-						theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-					}
-				}
-				return theRequest;
-			},
-			render: function() {
-				return ( < div />);
-				}
-
+	getInitialState: function() {
+		$('#log').text($('#log').text() + "| begin");
+		return {
+			code: '',
+			isLogin: false
+		};
+	},
+	componentWillMount: function() {
+		var req = new Object();
+		req = this.getRequest();
+		var code = req['code'];
+		if (code != '' && code != undefined) {
+			$('#log').text($('#log').text() + "| code is ok");
+			this.setState({
+				code: code,
+				isLogin: true
 			});
+		};
+	},
+	componentDidMount: function() {
+		$('#log').text($('#log').text() + "| begin to get from php" + this.state.code + "    " + this.state.isLogin);
+		if (this.state.isLogin) {
+			$.post("http://pictoshare.net/dev/build/php/oauth2_sub.php", {
+					code: this.state.code
+				},
+				function(data, status) {
+						$('#log').text($('#log').text() + "|   OK"+status);
+						var arry = data.split(":");
+						var subscribe = arry[3];
+						this.localSave(arry[2], arry[3], arry[0], arry[1]);
+						$('#log').text($('#log').text() + "|  data is ok =" + arry[2]);
+						if (subscribe == 0 && subscribe != '' && subscribe != undefined && subscribe != 'undefined') {
+							document.location = "http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyNzE3NjM1Nw==&scene=110#&wechat_redirect";
+						} else {
+							browserHistory.replace('/join');
+							$('#log').text($('#log').text() + "|  all is ok");
+						}
+				});
 
-		module.exports = wxLogin;
+
+			//$('#log').text($('#log').text() + "| gggggg" + this.state.code + "    " + this.state.isLogin);
+			// $
+			// 	.ajax({
+			// 		async: true,
+			// 		url: "http://pictoshare.net/dev/build/php/oauth2_sub.php",
+			// 		type: "GET",
+			// 		data: {
+			// 			code: this.state.code
+			// 		},
+			// 		timeout: 5000,
+			// 		success: function(result) {
+			// 			$('#log').text($('#log').text() + "|   OK");
+			// 			var arry = result.split(":");
+			// 			var subscribe = arry[3];
+			// 			this.localSave(arry[2], arry[3], arry[0], arry[1]);
+			// 			$('#log').text($('#log').text() + "|  data is ok =" + arry[2]);
+			// 			if (subscribe == 0 && subscribe != '' && subscribe != undefined && subscribe != 'undefined') {
+			// 				document.location = "http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyNzE3NjM1Nw==&scene=110#&wechat_redirect";
+			// 			} else {
+			// 				browserHistory.replace('/join');
+			// 				$('#log').text($('#log').text() + "|  all is ok");
+			// 			}
+			// 		},
+			// 		error: function() {
+			// 			$('#log').text($('#log').text() + "|  请求错误");
+			// 		}
+			// 	}.bind(this));
+		} else {
+			//修改授权地址
+			document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+		}
+	},
+	localSave: function(n, s, o, t) {
+		if (typeof(Storage) !== "undefined") {
+			sessionStorage.setItem("nickname", n);
+			sessionStorage.setItem("subscribe", s);
+			sessionStorage.setItem("openid", o);
+			sessionStorage.setItem("accesstoken", t);
+		}
+	},
+	getRequest: function() {
+		var url = document.location.search;
+		var theRequest = new Object();
+		var strs;
+		if (url.indexOf("?") != -1) {
+			var str = url.substr(1);
+			strs = str.split("&");
+			for (var i = 0; i < strs.length; i++) {
+				theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+			}
+		}
+		return theRequest;
+	},
+	render: function() {
+		return ( < div / > );
+	}
+
+});
+
+module.exports = wxLogin;
