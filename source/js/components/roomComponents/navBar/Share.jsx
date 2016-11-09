07e5a46743f78694ee88de77c1cabb9a7f29ccbd
xiaoyaw <wll77wang@163.com>
2016-11-09 10:52:01 +0800

    react:动态分享测试
    Change-Id: I3814d408c8b60ee1742b4fd3fbe7e089e93317b9

--- source/js/components/roomComponents/navBar/Share.jsx
+++ source/js/components/roomComponents/navBar/Share.jsx
@@ -17,6 +17,7 @@ var Share = React.createClass({
 		};
 	},
 	componentWillMount: function() {
+		console.log('will');
 		var thiz = this;
 		$.ajax({
 			async: false,
@@ -33,33 +34,31 @@ var Share = React.createClass({
 					timestamp = arry[1],
 					noncestr = arry[2],
 					signature = arry[3];
-				thiz.setState({
-					appid: appid,
-					timestamp: timestamp,
-					noncestr: noncestr,
-					signature: signature
+					console.log('sucess   '+signature);
+				wx.config({
+					debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					appId: appId, // 必填，公众号的唯一标识
+					timestamp: timestamp, // 必填，生成签名的时间戳
+					nonceStr: noncestr, // 必填，生成签名的随机串
+					signature: signature, // 必填，签名，见附录1
+					jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 				});
+
 			},
 			componentDidMount: function() {
+				console.log('did');
 				var thiz = this;
 				if (this.isMounted()) {
+					console.log('Mounted');
 					//微信分享接口
-					wx.config({
-						debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-						appId: this.state.appId, // 必填，公众号的唯一标识
-						timestamp: this.state.timestamp, // 必填，生成签名的时间戳
-						nonceStr: this.state.noncestr, // 必填，生成签名的随机串
-						signature: this.state.signature, // 必填，签名，见附录1
-						jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-					});
-
-					this.deal_wx_interface();
 					$('#share').click(function() {
+						console.log('clicked');
 						$('#myInput').modal('toggle');
 					});
 
 					$('#sureMsg').click(
 						function() {
+							console.log('sure');
 							var msg = $('#shareMsg').val();
 							if (msg != undefined) {
 								var strs = msg.split('&');
@@ -73,7 +72,7 @@ var Share = React.createClass({
 											desc: msg
 										});
 										thiz.deal_wx_interface();
-
+										console.log('desc=1  '+thiz.state.desc);
 										break;
 									default:
 										var req = new Object();
@@ -118,13 +117,14 @@ var Share = React.createClass({
 				}
 			},
 			deal_wx_interface: function() {
+				console.log('deal');
 				//验证签名，监听分享
 				var title = this.state.title,
 					desc = this.state.desc,
 					imgurl = this.state.imgUrl,
 					type = this.state.type,
 					dataUrl = this.state.dataUrl;
-
+					console.log('desc+deal  '+this.state.desc);
 				wx.ready(function() {
 					// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 					wx.onMenuShareAppMessage({
@@ -198,7 +198,7 @@ var Share = React.createClass({
 				});
 				wx.error(function(res) {
 
-					console.log('签名失败');
+					console.log('room签名失败');
 					// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
 
 				});
