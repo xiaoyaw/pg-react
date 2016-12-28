14fd4018497c2b2705f181f096675bf39234ba78
xiaoyaw <wll77wang@163.com>
2016-12-28 15:07:51 +0800

    react:公众号动态指定，修改bugthis关键字
    Change-Id: I16f6364e21ea4b1e938a13cca05e92f6e0740d0e

--- build/js/app.js
+++ build/js/app.js
@@ -28795,64 +28795,68 @@ var wxLogin = React.createClass({
 			release: 'dev'
 		};
 	},
-	componentWillMount: function componentWillMount() {
-		var req = new Object();
-		req = this.getRequest();
-		var code = req['code'];
-		var appid = req['state'];
-		if (code != '' && code != undefined && appid != '' && appid != undefined) {
-			this.setState({
-				code: code,
-				isLogin: true,
-				state: appid
-			});
-			sessionStorage.setItem("appid", this.state.appid);
-		};
-	},
 	componentDidMount: function componentDidMount() {
 		if (this.isMounted()) {
 			if (sessionStorage.nickname) {
 				//本地有则为返回，跳转回/join，否则第一次加入，需请求数据
 				_reactRouter.hashHistory.replace('/join');
 			} else {
-				if (this.state.isLogin) {
-					var cc = this.state.code;
-					var appid = this.this.appid;
-					$.ajax({
-						async: false,
-						url: "php/oauth2_sub.php",
-						type: "GET",
-						data: {
-							code: cc,
-							appid: appid
-						},
-						timeout: 5000,
-						success: function (result) {
-							var arry = result.split(":");
-							var subscribe = arry[3];
-							this.localSave(arry[2], arry[3], arry[0], arry[1]);
-							if (subscribe == 0 && subscribe != '' && subscribe != undefined && subscribe != 'undefined') {
-								document.location = "http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=" + arry[4] + "==&scene=110#&wechat_redirect";
-							} else {
-								_reactRouter.hashHistory.replace('/join');
-							}
-						}.bind(this)
+				var req = new Object();
+				req = this.getRequest();
+				var code = req['code'];
+				var appid = req['state'];
+				if (code != '' && code != undefined && appid != '' && appid != undefined) {
+					this.setState({
+						code: code,
+						isLogin: true,
+						state: appid
+					}, function () {
+						this.getuserinfo();
+						sessionStorage.setItem("appid", this.state.appid);
 					});
-				} else {
-					switch (this.state.appid) {
-						case 'wxe818778f16e4400d':
-							document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2f' + this.state.release + '%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-							break;
-						case 'wx6573103bb78bec40':
-							document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6573103bb78bec40&redirect_uri=http%3a%2f%2fwww.pictoshare.net%2f' + this.state.release + '%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-							break;
-					}
-					//修改授权地址wx6573103bb78bec40
-					//document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-					//e课
-					//document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6573103bb78bec40&redirect_uri=http%3a%2f%2fwww.pictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-				}
+				};
 			}
+		}
+	},
+	getuserinfo: function getuserinfo() {
+		if (this.state.isLogin) {
+			var cc = this.state.code;
+			var appid = this.state.appid;
+			$.ajax({
+				async: false,
+				url: "php/oauth2_sub.php",
+				type: "GET",
+				data: {
+					code: cc,
+					appid: appid
+				},
+				timeout: 5000,
+				success: function (result) {
+					var arry = result.split(":");
+					var subscribe = arry[3];
+					this.localSave(arry[2], arry[3], arry[0], arry[1]);
+					if (subscribe == 0 && subscribe != '' && subscribe != undefined && subscribe != 'undefined') {
+						document.location = "http://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=" + arry[4] + "==&scene=110#&wechat_redirect";
+					} else {
+						_reactRouter.hashHistory.replace('/join');
+					}
+				}.bind(this)
+			});
+		} else {
+			switch (this.state.appid) {
+				//奕甲
+				case 'wxe818778f16e4400d':
+					document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2f' + this.state.release + '%2fbuild&response_type=code&scope=snsapi_userinfo&state=' + this.state.appid + '#wechat_redirect';
+					break;
+				//e课
+				case 'wx6573103bb78bec40':
+					document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6573103bb78bec40&redirect_uri=http%3a%2f%2fwww.pictoshare.net%2f' + this.state.release + '%2fbuild&response_type=code&scope=snsapi_userinfo&state=' + this.state.appid + '#wechat_redirect';
+					break;
+			}
+			//修改授权地址wx6573103bb78bec40
+			//document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe818778f16e4400d&redirect_uri=http%3a%2f%2fpictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+			//e课
+			//document.location = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6573103bb78bec40&redirect_uri=http%3a%2f%2fwww.pictoshare.net%2fdev%2fbuild&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 		}
 	},
 	localSave: function localSave(n, s, o, t) {
